7f4b685c3154f6e35c45abeebc58fb7ea1c5e8ec
Sam Kyu, Kang <sky4411v@gmail.com>
2018-05-02 10:54:58 +0900

    데이터 업데이트

---
+++ FireChat/FireChat/View/ChatListCell.m
@@ -0,0 +1,24 @@
+//
+//  ChatListCell.m
+//  FireChat
+//
+//  Created by Samuel K on 2018. 4. 26..
+//  Copyright © 2018년 Samuel K. All rights reserved.
+//
+
+#import "ChatListCell.h"
+
+@implementation ChatListCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
